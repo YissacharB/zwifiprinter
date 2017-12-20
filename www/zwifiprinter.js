b7e4d64ba36ea30d95d082757dfa4e4010c5281e
jmier <34724834+jmier@users.noreply.github.com>
2017-12-20 17:54:32 -0500

    Update zwifiprinter.js

--- www/zwifiprinter.js
+++ www/zwifiprinter.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
 exports.find = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'find', []);
+    cordova.exec(successCallback, errorCallback, 'ZebraWiFiPrinter', 'find', []);
 };
 
